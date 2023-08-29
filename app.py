f0737ed0cee5721d40b7bdc148c68e88c7d36821
Om Uskaikar <117098130+omuskaikar@users.noreply.github.com>
2023-08-30 00:03:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ def conversational_chat(query, docs, chain):
 
 
 def delete_pickle_files():
-    # Iterate through all files in the current directory
     for filename in os.listdir('.'):
         if filename.endswith('.pkl'):
             os.remove(filename)
@@ -29,65 +28,72 @@ def main():
                 "<h1 style='text-align: center;margin: 10px 0px 10px 0px;background-color:black;"
                 "border-radius:7px ; color: white;position: relative;z-index:1000'>"
                 "Chat with PDF 📄</h1>", unsafe_allow_html=True)
-    pdf = st.sidebar.file_uploader("Upload your PDF", type='pdf')
-
-    if pdf is not None:
-        reader = PdfReader(pdf)
-        raw_text = ''
-        for i, page in enumerate(reader.pages):
-            text = page.extract_text()
-            if text:
-                raw_text += text
-
-        text_splitter = CharacterTextSplitter(
-            separator="\n",
-            chunk_size=1000,
-            chunk_overlap=100,
-            length_function=len
-        )
-        texts = text_splitter.split_text(raw_text)
-
-        store_name = pdf.name[:-4]
-
+    os.environ['OPENAI_API_KEY'] = "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"
+    pdf_list = st.sidebar.file_uploader("Upload your PDF", type='pdf', accept_multiple_files=True)
+    if len(pdf_list) > 0:
+        texts = []
+        process = st.sidebar.button("Process")
+        store_name = "embeddings"
         if os.path.exists(f"{store_name}.pkl"):
             with open(f"{store_name}.pkl", "rb") as f:
                 docsearch = pickle.load(f)
-        else:
+        if process:
+            delete_pickle_files()
+            raw_text = ''
+            for pdf in pdf_list:
+                reader = PdfReader(pdf)
+                for i, page in enumerate(reader.pages):
+                    text = page.extract_text()
+                    if text:
+                        raw_text += text
+
+            text_splitter = CharacterTextSplitter(
+                separator="\n",
+                chunk_size=1000,
+                chunk_overlap=100,
+                length_function=len
+            )
+            texts = text_splitter.split_text(raw_text)
+
+            # Pass embeddings parameter
+
             embeddings = OpenAIEmbeddings()  # Instantiate OpenAIEmbeddings
-            docsearch = FAISS.from_texts(texts, embedding=embeddings)  # Pass embeddings parameter
+            docsearch = FAISS.from_texts(texts, embedding=embeddings)
             with open(f"{store_name}.pkl", "wb") as f:
                 pickle.dump(docsearch, f)
 
-        chain = load_qa_chain(llm=OpenAI(), chain_type="stuff")
-        if 'history' not in st.session_state:
-            st.session_state['history'] = []
+        if os.path.exists(f"{store_name}.pkl"):
+            chain = load_qa_chain(llm=OpenAI(), chain_type="stuff")
+            if 'history' not in st.session_state:
+                st.session_state['history'] = []
 
-        if 'generated' not in st.session_state:
-            st.session_state['generated'] = ["Hello ! Ask me anything about " + pdf.name + " 🤗"]
+            if 'generated' not in st.session_state:
+                st.session_state['generated'] = ["Hello ! Ask me anything about the uploaded pdf's" + " 🤗"]
 
-        if 'past' not in st.session_state:
-            st.session_state['past'] = ["Hey ! 👋"]
+            if 'past' not in st.session_state:
+                st.session_state['past'] = ["Hey ! 👋"]
 
-        response_container = st.container()
-        container = st.container()
+            response_container = st.container()
+            container = st.container()
 
-        user_input = st.chat_input("Your Query:")
+            user_input = st.chat_input("Your Query:")
 
-        if user_input:
-            # Process user input and generate bot response
-            docs = docsearch.similarity_search(query=user_input, k=3)
-            output = conversational_chat(user_input, docs, chain)
+            if user_input:
+                # Process user input and generate bot response
+                docs = docsearch.similarity_search(query=user_input, k=3)
+                output = conversational_chat(user_input, docs, chain)
 
-            st.session_state['past'].append(user_input)
-            st.session_state['generated'].append(output)
+                st.session_state['past'].append(user_input)
+                st.session_state['generated'].append(output)
 
-        if st.session_state['generated']:
-            with response_container:
-                for i in range(len(st.session_state['generated'])):
-                    message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile")
-                    message(st.session_state["generated"][i], key=str(i), avatar_style="bottts")
+            if st.session_state['generated']:
+                with response_container:
+                    for i in range(len(st.session_state['generated'])):
+                        message(st.session_state["past"][i], is_user=True, key=str(i) + '_user',
+                                avatar_style="big-smile")
+                        message(st.session_state["generated"][i], key=str(i), avatar_style="bottts")
 
-        atexit.register(delete_pickle_files)
+    atexit.register(delete_pickle_files)
 
 
 if __name__ == "__main__":
